cdec45ccaa5e6921e5e1bed3112975e1ea577405
SantosHM <dosantoshm@gmail.com>
2018-11-11 13:15:29 -0500

    Cargar css

--- functions.php
+++ functions.php
@@ -2,7 +2,15 @@
 
 function lapizzeria_styles()
 {
-	wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0');
+	// REGISTRAR LOS ESTILOS
+	wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.css', array(), '8.0.1');
+
+	//que cargue primero normalize
+	wp_register_style('style', get_template_directory_uri() . '/style.css', array('normalize'), '1.0');
+	
+	// LLAMAR A LOS ESTILOS
+	wp_enqueue_style('normalize');
+	wp_enqueue_style('style');
 }
 
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
